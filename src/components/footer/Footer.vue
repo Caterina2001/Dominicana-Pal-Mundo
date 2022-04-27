d1ee167bf1926e82c03e010759c2bf475a2344bb
Caterina2001 <rosariocaterina7@gmail.com>
2022-04-26 21:32:20 -0400

    HomeView

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -37,11 +37,6 @@ export default Vue.extend({
 </script>
 
 <style scoped>
-.footer{
-    width:100%;
-    height:40px;
-    position: absolute;
-    bottom: 0;
-}
+
 
 </style>
--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -1,19 +1,394 @@
 <template>
-  <div>
-    <h1>Home View</h1>
 
-    <p>Esta vista está disponible gracias al routing, es contenido dinámico </p>
+  <body>
+
+      
+      <div id="">
+          <div class="jumper">
+              <div></div>
+              <div></div>
+              <div></div>
+          </div>
+      </div>  
+      <div class=" header-text">
+        <div class="dive">
+          <div class="owl-banner owl-carousel owl-loaded owl-drag">      
+          <div class=""><div class="owl-item cloned" style="width: 412px; margin-right: 10px;"><div class="item">
+              <img src="assets/images/img1.png" alt="">
+              <div class="item-content">
+                <div class="main-content">
+                  <div class="meta-category">
+                    <span><h3>Espaillat</h3></span>
+                  </div>
+                  <a href="post-details-hongo.html"><h4>Hongo Magico</h4></a>
+                  <ul class="post-info">
+                    <li><a href="#">Hoteles</a></li>
+                    <li><a href="#">Experiencias</a></li>
+                    <li><a href="#">Restaurantes</a></li>
+                  </ul>
+                </div>             
+              </div>
+            
+            </div></div><div class="owl-item cloned" style="width: 412px; margin-right: 10px;"><div class="item">
+              <img src="assets/images/img5.png" alt="">
+              <div class="item-content">
+                <div class="main-content">
+                  <div class="meta-category">
+                    <span><h3>Samana</h3></span>
+                  </div>
+                  <a href="post-details-fronton.html"><h4>Playa Frontón</h4></a>
+                  <ul class="post-info">
+                    <li><a href="#">Hoteles</a></li>
+                    <li><a href="#">Experiencias</a></li>
+                    <li><a href="#">Restaurantes</a></li>
+                  </ul>
+                </div>
+              </div>
+
+            </div></div><div class="owl-item active" style="width: 412px; margin-right: 10px;"><div class="item">
+              <img src="assets/images/img4.png" alt="">
+              <div class="item-content">
+                <div class="main-content">
+                  <div class="meta-category">
+                    <span><h3>San Juan</h3></span>
+                  </div>
+                  <a href="post-details-sanjuan.html"><h4>Cañón del rio blanco</h4></a>
+                  <ul class="post-info">
+                    <li><a href="hotel-sanjuan.html">Hoteles</a></li>
+                    <li><a href="experiencia-sanjuan.html">Experiencias</a></li>
+                    <li><a href="restaurante-sanjuan.html">Restaurantes</a></li>
+                  </ul>
+                </div>
+              </div>
+
+            
+              
+
+
+            </div></div></div></div></div>
+        </div>
+
+<!-- 
+    <div class="main-banner header-text">
+      <div class="container-fluid">
+        <div class="owl-banner owl-carousel owl-loaded owl-drag">
+          <div class="item">
+            <img src="assets/images/img2.png" alt="">
+            <div class="item-content">
+              <div class="main-content">
+                <div class="meta-category">
+                  <span><h3>Punta Cana</h3></span>
+                </div>
+                <a href="post-details-hoyo.html"><h4>Hoyo Claro</h4></a>
+                <ul class="post-info">
+                  <li><a href="hotel-hoyo.html">Hoteles</a></li>
+                  <li><a href="#">Experiencias</a></li>
+                  <li><a href="#">Restaurantes</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+      
+          <div class="item">
+            <img src="assets/images/img4.png" alt="">
+            <div class="item-content">
+              <div class="main-content">
+                <div class="meta-category">
+                  <span><h3>San Juan</h3></span>
+                </div>
+                <a href="post-details-sanjuan.html"><h4>Cañón del rio blanco</h4></a>
+                <ul class="post-info">
+                  <li><a href="hotel-sanjuan.html">Hoteles</a></li>
+                  <li><a href="experiencia-sanjuan.html">Experiencias</a></li>
+                  <li><a href="restaurante-sanjuan.html">Restaurantes</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="item">
+            <img src="assets/images/img3.png" alt="">
+            <div class="item-content">
+              <div class="main-content">
+                <div class="meta-category">
+                  <span><h3>Pedernales</h3></span>
+                </div>
+                <a href="post-details.html"><h4>Trudille</h4></a>
+                <ul class="post-info">
+                  <li><a href="#">Hoteles</a></li>
+                  <li><a href="#">Experiencias</a></li>
+                  <li><a href="#">Restaurantes</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="item">
+            <img src="assets/images/img1.png" alt="">
+            <div class="item-content">
+              <div class="main-content">
+                <div class="meta-category">
+                  <span><h3>Samana</h3></span>
+                </div>
+                <a href="post-details.html"><h4>Cayo Levantado</h4></a>
+                <ul class="post-info">
+                  <li><a href="#">Hoteles</a></li>
+                  <li><a href="#">Experiencias</a></li>
+                  <li><a href="#">Restaurantes</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="item">
+            <img src="assets/images/img6.png" alt="">
+            <div class="item-content">
+              <div class="main-content">
+                <div class="meta-category">
+                  <span><h3>Espaillat</h3></span>
+                </div>
+                <a href="post-details-hongo.html"><h4>Hongo Magico</h4></a>
+                <ul class="post-info">
+                  <li><a href="#">Hoteles</a></li>
+                  <li><a href="#">Experiencias</a></li>
+                  <li><a href="#">Restaurantes</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          
+          <div class="item">
+            <img src="assets/images/img5.png" alt="">
+            <div class="item-content">
+              <div class="main-content">
+                <div class="meta-category">
+                  <span><h3>Samana</h3></span>
+                </div>
+                <a href="post-details-fronton.html"><h4>Playa Frontón</h4></a>
+                <ul class="post-info">
+                  <li><a href="#">Hoteles</a></li>
+                  <li><a href="#">Experiencias</a></li>
+                  <li><a href="#">Restaurantes</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        
+        </div>
+      </div>
+    </div>
+   -->
+
+      <section class="call-to-action">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="main-content">
+              <div class="row">
+                <div class="col-lg-8">
+                  <span>Dominicana pal mundo</span>
+                  <h4>Disponible ahora la aplicación para movil</h4>
+                </div>
+                <div class="col-lg-4">
+                  <div class="main-button">
+                    <a rel="nofollow" href="https://templatemo.com/tm-551-stand-blog" target="_parent">Descarga aqui!</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+    <section class="blog-posts">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8">
+            <div class="all-blog-posts">
+              <div class="row">
+                <div class="col-lg-12">
+                  <div class="blog-post">
+                    <div class="blog-thumb">
+                      <img src="assets/images/padrenuestro.png" alt="">
+                    </div>
+                    <div class="down-content">
+                      <span>Bayahibe</span>
+                      <a href="post-details.html"><h4>Cueva Padre Nuestro</h4></a>
+                      <ul class="post-info">
+                      </ul>
+                      <p>Recorre el sendero ecológico y arqueológico Padre Nuestro en el parque nacional del Este, en Bayahibe, y admira los espectaculares lugares por los que te llevará. El bosque tropical húmedo, que te acompañará todo el camino, te mostrará numerosas especies endémicas de aves, reptiles y mariposas que conviven en un paisaje compuesto por piedra caliza.</p>
+                      <div class="post-options">
+                        <div class="row">
+                          <div class="col-6">
+                            <ul class="post-tags">
+                              <li><i class="fa fa-tags"></i></li>
+                              <li><a href="#">Cueva</a>,</li>
+                              <li><a href="#">Natura</a></li>
+                            </ul>
+                          </div>
+                          <div class="col-6">
+                            <ul class="post-share">
+                              <li><i class="fa fa-share-alt"></i></li>
+                              <li><a href="#">Facebook</a>,</li>
+                              <li><a href="#"> Instagram</a></li>
+                            </ul>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>               
+                </div> 
+                <div class="col-lg-12">
+                  <div class="blog-post">
+                    <div class="blog-thumb">
+                      <img src="assets/images/paellero.png" alt="">
+                    </div>
+                    <div class="down-content">
+                      <span>La Vacama</span>
+                      <a href="post-details.html"><h4>Paellero Valenciano</h4></a>
+                      <ul class="post-info">
+                      </ul>
+                      <p>You can support us by contributing a little via PayPal. Please contact <a rel="nofollow" href="https://templatemo.com/contact" target="_parent">TemplateMo</a> via Live Chat or Email. If you have any question or feedback about this template, feel free to talk to us. Also, you may check other CSS templates such as <a rel="nofollow" href="https://templatemo.com/tag/multi-page" target="_parent">multi-page</a>, <a rel="nofollow" href="https://templatemo.com/tag/resume" target="_parent">resume</a>, <a rel="nofollow" href="https://templatemo.com/tag/video" target="_parent">video</a>, etc.</p>
+                      <div class="post-options">
+                        <div class="row">
+                          <div class="col-6">
+                            <ul class="post-tags">
+                              <li><i class="fa fa-tags"></i></li>
+                              <li><a href="#">Playa</a>,</li>
+                              <li><a href="#">Arena</a></li>
+                            </ul>
+                          </div>
+                          <div class="col-6">
+                            <ul class="post-share">
+                              <li><i class="fa fa-share-alt"></i></li>
+                              <li><a href="#">Facebook</a>,</li>
+                              <li><a href="#">Instagram</a></li>
+                            </ul>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-12">
+                  <div class="blog-post">
+                    <div class="blog-thumb">
+                      <img src="assets/images/cañon.png" alt="">
+                    </div>
+                    <div class="down-content">
+                      <span>Jamao</span>
+                      <a href="post-details.html"><h4>Cañón de Arroyo Frío</h4></a>
+                      <ul class="post-info">
+                        
+                      </ul>
+                      <p>El Cañón de Arroyo Frío se encuentra en Jamao al Norte, en la Provincia Espaillat. Es un destino único en el país y  es una de las rutas establecidas de barranquismo más largas en la República Dominicana. El recorrido de 3.5 km se hace a través de 18 cascadas y más de 60 charcos. Es un sueño para aventureros y un lugar espectacular.
+                      </p>
+                      <div class="post-options">
+                        <div class="row">
+                          <div class="col-6">
+                            <ul class="post-tags">
+                              <li><i class="fa fa-tags"></i></li>
+                              <li><a href="#">Rio</a>,</li>
+                              <li><a href="#">Photop</a></li>
+                            </ul>
+                          </div>
+                          <div class="col-6">
+                            <ul class="post-share">
+                              <li><i class="fa fa-share-alt"></i></li>
+                              <li><a href="#">Facebook</a>,</li>
+                              <li><a href="#">Instagram</a></li>
+                            </ul>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-12">
+                  <div class="main-button">
+                    <a href="./reservas/index.html">Crea un Tour Personalizado</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="sidebar">
+              <div class="row">
+                <div class="col-lg-12">
+                  <div class="sidebar-item search">
+                    <form id="search_form" name="gs" method="GET" action="#">
+                      <input type="text" name="q" class="searchText" placeholder="Buscar..." autocomplete="on">
+                    </form>
+                  </div>
+                </div>
+                <div class="col-lg-12">
+                  <div class="sidebar-item categories">
+                    <div class="sidebar-heading">
+                      <h2>Categorias</h2>
+                    </div>
+                    <div class="content">
+                      <ul>
+                        <li><a href="#">- Nature </a></li>
+                        <li><a href="#">- Awesome </a></li>
+                        <li><a href="#">- Creative </a></li>
+                        <li><a href="#">- Responsive </a></li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-12">
+                  <div class="sidebar-item tags">
+                    <div class="sidebar-heading">
+                      <h2>Etiquetas</h2>
+                    </div>
+                    <div class="content">
+                      <ul>
+                        <li><a href="#">Playa</a></li>
+                        <li><a href="#">Cueva</a></li>
+                        <li><a href="#">Arroyo</a></li>
+                        <li><a href="#">Lago</a></li>
+                        <li><a href="#">Rio</a></li>
+                        <li><a href="#">Campaña</a></li>
+                        <li><a href="#">Natura</a></li>
+                        <li><a href="#">Jardin</a></li>
+                        <li><a href="#">Acuario</a></li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          
+        </div>
+      </div>
+    </section>
+
+
+
+  </body>
   
-  </div>
   
 </template>
 
 
 
+
 <script lang="ts">
+
 import Vue from 'vue';
 
 export default Vue.extend({
  
 });
 </script>
+
+<style scoped>
+.dive{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+</style>
